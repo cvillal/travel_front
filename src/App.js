0afedee1678b56fed0f8c27375f0dd2b33e18c54
Carolina Villalobos <cvillal@ncsu.edu>
2022-09-28 22:58:57 -0400

    added update to app.js, edits go through successfully but couldnt get edit button to toggle yet

--- src/App.js
+++ src/App.js
@@ -17,8 +17,8 @@ const [newPlace, setNewPlace] = useState();
 const [newStay, setNewStay] = useState(false)
 const [newLink, setNewLink] = useState()
 const [newNotes, setNewNotes] = useState()
-const [newRating, setNewRating] = useState()
-const [newUsername, setNewUsername] = useState()
+const [newRating, setNewRating] = useState(0)
+const [newUsername, setNewUsername] = useState(' ')
 const [newLat, setNewLat] = useState(0)
 const [newLng, setNewLng] = useState (0)
 //update states
@@ -66,12 +66,37 @@ const handleCreate = (addTC) => {
   setDisplayAddTravelCard(!displayAddTravelCard)
 }
 
+//update travel card
 
+const updateTravelCard = (travelData) => {
+  axios.put(`http://localhost:3000/tc/${travelData._id}`,
+    {
+      image: newImage,
+      place: newPlace,
+      stay: newStay,
+      link: newLink,
+      notes:newNotes,
+      rating:newRating,
+      username:newUsername,
+      lat: newLat,
+      lng: newLng
+    }
+  ).then(() => {
+    axios
+      .get('http://localhost:3000/tc')
+      .then((response) => {
+        setNewTravelCard(response.data)
+      })
+  })
+}
 
 //displays
 
 const showAddTravelCard = () => {
   setDisplayAddTravelCard(!displayAddTravelCard)
+}
+const showEditTravelCard = (tc) => {
+  document.getElementById('edittc'+tc._id).classList.toggle('showhide');
 }
 
 //new card changes
@@ -82,7 +107,7 @@ const submitNewPlace = (event) => {
   setNewPlace(event.target.value)
 }
 const submitNewStay = (event) => {
-  setNewStay(event.target.value)
+  setNewStay(event.target.checked)
 }
 const submitNewLink = (event) => {
   setNewLink(event.target.value)
@@ -121,7 +146,7 @@ const submitNewLng = (event) => {
               <label>friend's place</label><input type='checkbox' value='false' onChange={submitNewStay}/><br/>
               <details><summary><label>Have a link of where you stayed?</label></summary><input type='text' onChange={submitNewLink}/></details>
               <label>Notes on the place:</label><input type='text' onChange={submitNewNotes}/><br/>
-              <label>Rating:</label><input type='text' onChange={submitNewRating}/><br/>
+              <label>Rating:</label><input type='number' min='0' max='5' onChange={submitNewRating}/><br/>
               <label>Username:</label><input type='text' onChange={submitNewUsername}/><br/>
 
               <input className='inputbutton' type='submit' value='Post New Travel Card'/>
@@ -135,17 +160,39 @@ const submitNewLng = (event) => {
         {travelCard.map((tc) => {
           return (
             <div key={tc._id}>
-            <img src={tc.image}/>
-            <h5>What's this place? {tc.place}</h5>
-            <h5>Where'd you stay? {tc.stay}</h5>
-            <h5>{tc.link}</h5>
-            <h5>My thoughts and tips: <br/>{tc.notes}</h5>
-            <h5>Rating: {tc.rating}</h5>
-            <h5>Username: {tc.username}</h5>
+              <img src={tc.image} style={{height:"300px"}}/>
+              <h5>What's this place? {tc.place}</h5>
+              <h5>Where'd you stay? {tc.stay}</h5>
+              <h5>{tc.link}</h5>
+              <h5>My thoughts and tips: <br/>{tc.notes}</h5>
+              <h5>Rating: {tc.rating}</h5>
+              <h5>Username: {tc.username}</h5>
+              
+              <>
+              <button onClick={() => {showEditTravelCard(tc)}}>edit</button>
+              <section id={'edittc'+tc._id}>
+              <form  onSubmit={() => {updateTravelCard(tc)}}>
+                <label>Image link:</label><input type='text' placeholder={tc.image} onChange={submitNewImage}/><br/>
+                <label>What's this place?</label><input type='text' placeholder={tc.place} onChange={submitNewPlace}/><br/>
+                <label>Where'd you stay? <br/> airbnb </label><input type='checkbox' value='false'  onChange={submitNewStay}/>
+                <label>hotel</label><input type='checkbox' value='false' onChange={submitNewStay}/>
+                <label>friend's place</label><input type='checkbox' value='false'  onChange={submitNewStay}/><br/>
+                <details><summary><label>Have a link of where you stayed?</label></summary><input type='text' placeholder={tc.link} onChange={submitNewLink}/></details>
+                <label>Notes on the place:</label><input type='text' placeholder={tc.notes} onChange={submitNewNotes}/><br/>
+                <label>Rating:</label><input type='number' min='0' max='5' placeholder={tc.rating} onChange={submitNewRating}/><br/>
+                <label>Username:</label><input type='text' placeholder={tc.username} onChange={submitNewUsername}/><br/>
+
+                <input className='inputbutton' type='submit' value='Update Travel Card' onClick={(e) => {updateTravelCard(tc)}}/>
+              </form>
+              </section>
+              </>
             </div>
+            
           )
         })}
       </div>
+
+
     </div>
   );
 }
